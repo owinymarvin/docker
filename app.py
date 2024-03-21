b822489b59e63ba08e08c295a69d1983f4991bdf
OWINY DOLLO MARVIN <owinymarvin@gmail.com>
2024-03-21 17:01:33 +0300

    py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 from flask import Flask, render_template, request
 from keras.models import load_model
 from keras.preprocessing import image
